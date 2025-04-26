a8617b63634cbce27e18e5465c9d2c62a26fa432
Karanpreet Singh <70535079+Karangarha@users.noreply.github.com>
2025-04-25 20:46:51 -0400

    Update forms

--- application/bp/authentication/forms/__init__.py
+++ application/bp/authentication/forms/__init__.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class RegisterForm(FlaskForm):
-    pass
-
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
